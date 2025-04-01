33c03c7a5fb2eca7f3631e18edddf0bc565a9753
Daniel <a01567295@tec.mx>
2025-04-01 17:11:03 -0600

    Update Tarjeta.js

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export function Tarjeta() {
+export function Tarjeta(props) {
   return (
-    <div>
-        Tarjeta de Personaje
-    </div>
+    <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.imagen} alt="pda logo" />
+          </figure>
+        </div>
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{props.nombre}</p>
+            <p className="subtitle is-6">{props.color} es mi color</p>
+          </div>
+        </div>
+      </div>
   )
 }
